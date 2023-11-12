979ee14130c3c346402b160753c77dc324ba3233
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2023-11-11 21:13:35 -0300

    vriação da rota put

--- routes/cadastro.js
+++ routes/cadastro.js
@@ -2,11 +2,10 @@ const express = require('express');
 const conectarBancoDados = require('../middlewares/configBD');
 const EsquemaCadastro = require('../models/modelCadastro')
 const router = express.Router();
-const app = express()
-app.use(express.json())
+
 
     // rota para cadastrar livro
-router.post('/cadastro', conectarBancoDados, async function (req, res) {
+    router.post('/cadastro', conectarBancoDados, async function (req, res) {
     try {
         // #swagger.tags = ['CADASTRO']
         let { titulo, paginas, isbn, editora } = req.body;
@@ -23,14 +22,31 @@ router.post('/cadastro', conectarBancoDados, async function (req, res) {
         )
 
     }
-
-})
+    })
 
     // rota para buscar todos os livros
     router.get('/busca', conectarBancoDados, async function (req, res) {
         try {
          
           const respostaBD = await EsquemaCadastro.find(req.params.id);
+      
+          res.status(200).json({
+            status: "OK",
+            statusMensagem: "Todos os livros cadastrados!.",
+            resposta: respostaBD
+          })
+      
+        } catch (error) {
+          return Error('Erro ao buscar, tente novamente !')
+        }
+      });
+
+
+      // rota para buscar livro individualmente por Id
+    router.get('/busca/:id', conectarBancoDados, async function (req, res) {
+        try {
+          const livroId = req.params.id
+          const respostaBD = await EsquemaCadastro.findOne({_id:livroId});
       
           res.status(200).json({
             status: "OK",
@@ -42,5 +58,29 @@ router.post('/cadastro', conectarBancoDados, async function (req, res) {
           return Error('Erro ao buscar, tente novamente !')
         }
       });
+
+
+       // rota para editar o livro
+       router.put('/editar/:id', conectarBancoDados, async function (req, res) {
+        try {
+            const livroId = req.params.id
+            const respostaBD = await EsquemaCadastro.findOne({_id:livroId});
+            let { titulo, paginas, isbn, editora } = req.body;
+            
+            const livroAtualizado = await EsquemaCadastro.updateOne({_id: livroId}, {livroId, titulo, paginas, isbn, editora});
+
+            res.status(200).json ({
+                status:"OK",
+                statusMensagem: "Livro atualizado!.",
+                resposta: respostaBD
+            })
+
+        } catch (error) {
+            return Error ('Erro ao atualizar, tente novamente.')
+        }
+       })
+
+
+
 
 module.exports = router;
