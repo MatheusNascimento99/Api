f975123310c63d8fd2884ddd79e5059f36bd99d0
Matheus Nascimento <139829100+MatheusNascimento99@users.noreply.github.com>
2024-07-04 10:21:29 -0300

    feat: ajustado link mongoDB

--- middlewares/configBD.js
+++ middlewares/configBD.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 async function conectarBancoDados(req = null, res = null, next = null) {
   try {
-    await mongoose.connect("mongodb+srv://matheuscrnascimento:123@cluster.qztpgwh.mongodb.net/?retryWrites=true&w=majority");
+    await mongoose.connect("mongodb+srv://matheuscrnascimento:<password>@cluster.qztpgwh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster");
     console.log('Conectado ao banco de dados!');
     try { next(); } catch { };
     return mongoose;
